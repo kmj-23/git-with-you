0059d36f42be4d793c88f1b9fb1b6c93925a19b4
devsyw <devsyw@gmail.com>
2024-12-16 20:12:41 +0900

    feature: print develop

--- src/main/java/com/example/gitprac/GitpracApplication.java
+++ src/main/java/com/example/gitprac/GitpracApplication.java
@@ -8,6 +8,7 @@ public class GitpracApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitpracApplication.class, args);
+        System.out.println("서영우가 쓴 프린트~!");
     }
 
 }
