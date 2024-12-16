a932d2950c5537f2bffd74d485fceda24a9ebd65
devsyw <devsyw@gmail.com>
2024-12-16 20:55:39 +0900

    feature: create 느낌표

--- src/main/java/com/example/gitprac/GitpracApplication.java
+++ src/main/java/com/example/gitprac/GitpracApplication.java
@@ -8,7 +8,7 @@ public class GitpracApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitpracApplication.class, args);
-        System.out.println("서영우가 쓴 프린트~!");
+        System.out.println("서영우가 쓴 프린트~!!!!!!!");
     }
 
 }
